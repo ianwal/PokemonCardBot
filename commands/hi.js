e4c871747a6ef770ea8dda99ada591f0079b67ce
Tommy <tommypakitup@gmail.com>
2023-03-16 06:19:26 -0700

    bare bones

---
+++ commands/hi.js
@@ -0,0 +1,11 @@
+const {SlashCommandBuilder} = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('hi')
+        .setDescription('says hi'),
+    async execute(interaction) {
+        await interaction.reply('Hello');
+        return;
+    }
+}
---
+++ index.js
@@ -0,0 +1,45 @@
+const {Client, Collection, Events, GatewayIntentBits} = require('discord.js');
+const {token} = require('./config.json');
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates]
+});
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required 'data' or 'execute' property.`);
+    }
+}
+
+client.once(Events.ClientReady, () => {
+    console.log(`${client.user.tag} is online`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+    if(!interaction.isChatInputCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+    if(!command) return;
+
+    try {   
+        await command.execute(interaction);
+    }
+    catch(error) {
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!' + error, ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!' + error, ephemeral: true });
+        }
+    }
+});
+
+client.login(token);
