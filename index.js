f518b4d4e341335916744c08a1003d02fc5727b4
lanechilders <lanechilders0404@gmail.com>
2023-03-16 06:28:25 -0700

    fs + path

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const {Client, Collection, Events, GatewayIntentBits} = require('discord.js');
 const {token} = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates]
