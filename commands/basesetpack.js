d7b666d426544fc143386e299393bbcd203eeff8
lanechilders <lanechilders0404@gmail.com>
2023-03-19 23:07:47 -0700

    used a for each loop to create a new json file with no basic energies

--- commands/basesetpack.js
+++ commands/basesetpack.js
@@ -3,7 +3,6 @@ const fs = require('node:fs');
 const obj = JSON.parse(fs.readFileSync('./baseset1.json'));
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle, ButtonInteraction, Message } = require('discord.js');
 const {Client, Collection, Events, GatewayIntentBits} = require('discord.js');
-const allCardsCopy = obj.slice(); // Copy of obj to remove cards from for removing duplicate card pulls
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMembers]
@@ -31,24 +30,30 @@ module.exports = {
     });
 
     /// For loop time
-    basicEnergy = []
+    const basicEnergy = []
+    const allCardsCopy = obj.slice(); // Copy of obj to remove cards from for removing duplicate card pulls
 
     obj.forEach((card, index) => {
         if (card.supertype === "Energy" && card.subtypes[0] === "Basic") {
             basicEnergy.push(index);
         }
     }); 
-    console.log(basicEnergy)
+
+    // remove energies from the juice
 
     value = Math.floor(Math.random() * basicEnergy.length);
     index = basicEnergy[value]
     await interaction.reply(obj[index].images.large);  // first card is guarenteed to be a basic energy
 
+    basicEnergy.forEach((card, index) => {
+        allCardsCopy.pop(basicEnergy[index], 1);
+    });
+
     let counter = 1;
     collector.on('collect', async i => { // on button press -> new card
     if (interaction.user.id === i.user.id) {   
         newValue = Math.floor(Math.random() * (101-0+1) + 0); // (max - min + 1) + min) -> array so subtract 1 from each value and 102 cards in base set
-        await interaction.editReply(obj[newValue].images.large);
+        await interaction.editReply(allCardsCopy[newValue].images.large);
         if (counter >=10) {                                         // 9 clicks till button is disabled (1 initial card + 9 more cards for a pack)
             row.components[0].setDisabled(true);
             await i.update({content: ' ', components: [row] });
@@ -68,6 +73,3 @@ module.exports = {
     }
 }
 
-// energyValues = [96,97,98,99,100,101];  // scuffed brute force to find all basic energies in json file
-//     value = Math.floor(Math.random() * energyValues.length);
-//     index = energyValues[value]
