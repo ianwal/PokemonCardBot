4d256d68b1d0130e5f95e796c2e9946a549ea3df
lanechilders <lanechilders0404@gmail.com>
2023-03-20 03:10:57 -0700

    first card = energy, next 8 are common/uncommon, last is rare or rare holo. (all equal chance odds)

--- commands/basesetpack.js
+++ commands/basesetpack.js
@@ -31,12 +31,17 @@ module.exports = {
 
     /// For loop time
     const basicEnergy = []
+    const commonsUncommons = []
+    const rareAndHolos = []
     const allCardsCopy = obj.slice(); // Copy of obj to remove cards from for removing duplicate card pulls
 
     obj.forEach((card, index) => {
         if (card.supertype === "Energy" && card.subtypes[0] === "Basic") {
             basicEnergy.push(index);
-        }
+        } else if (card.rarity === "Common" || card.rarity === "Uncommon") { 
+            commonsUncommons.push(index);
+        } else 
+            rareAndHolos.push(index);
     }); 
 
     // remove energies from the juice
@@ -46,19 +51,29 @@ module.exports = {
     await interaction.reply(obj[index].images.large);  // first card is guarenteed to be a basic energy
 
     basicEnergy.forEach((card, index) => {
-        allCardsCopy.pop(basicEnergy[index], 1);
-    });
+        allCardsCopy.pop();                 // scuffed method to remove energies since all basic energies are at the end of the json                                    
+    });       
+
+
+    let onlyCommonUncommon = allCardsCopy.slice()
+    let onlyRare = allCardsCopy.slice()
+
+    onlyCommonUncommon = onlyCommonUncommon.filter((element, index) => commonsUncommons.includes(index)); // Array with only commons/uncommons
+    onlyRare = onlyRare.filter((element, index) => rareAndHolos.includes(index)); // Array with only Rares /RareHolos 
 
     let counter = 1;
     collector.on('collect', async i => { // on button press -> new card
     if (interaction.user.id === i.user.id) {   
-        newValue = Math.floor(Math.random() * (101-0+1) + 0); // (max - min + 1) + min) -> array so subtract 1 from each value and 102 cards in base set
-        await interaction.editReply(allCardsCopy[newValue].images.large);
-        if (counter >=10) {                                         // 9 clicks till button is disabled (1 initial card + 9 more cards for a pack)
+        if (counter >= 9) {                                         // 9 clicks till button is disabled (1 initial card + 9 more cards for a pack)
+            newValue = Math.floor(Math.random() * (onlyRare.length));
+            await interaction.editReply(onlyRare[newValue].images.large);
             row.components[0].setDisabled(true);
             await i.update({content: ' ', components: [row] });
             collector.stop()
         } else {
+            newValue = Math.floor(Math.random() * (onlyCommonUncommon.length));
+            await interaction.editReply(onlyCommonUncommon[newValue].images.large);
+            allCardsCopy.splice(newValue, 1);
             counter++;
             await i.update({content: ' ', components: [row] });
         }
